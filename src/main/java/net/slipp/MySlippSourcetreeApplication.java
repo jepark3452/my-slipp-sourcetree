4047561deb0c1c72b347d5e7517e6308b412f749
DESKTOP-MGMHCD5\jepark <jepark3452@gmail.com>
2017-05-12 16:52:55 +0900

    initial commit

---
+++ src/main/java/net/slipp/MySlippSourcetreeApplication.java
@@ -0,0 +1,12 @@
+package net.slipp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySlippSourcetreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySlippSourcetreeApplication.class, args);
+	}
+}
